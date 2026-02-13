424da4d19151b02f1e247823cbc94a31dc1fd21d
mohdsajid9600 <mohdsajid9600@gmail.com>
2026-02-14 01:48:51 +0530

    add favicon

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://easytrip-api-kaeq.onrender.com',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
